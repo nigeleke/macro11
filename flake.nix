b0253167e1ddc6517fc67cd28adb4b942a32df13
Nigel Eke <nigeleke@gmail.com>
2023-08-17 18:08:43 +1000

    Add flake.nix devShell

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs = { nixpkgs, systems, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.vscode pkgs.cmake ];
+      };
+    };
+}
