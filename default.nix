70747953e1bb415c03bfe643a84b8d6d59c3edf2
Nigel Eke <nigeleke@gmail.com>
2023-08-18 18:21:54 +1000

    Add nix metadata 2

--- default.nix
+++ default.nix
@@ -1,14 +1,14 @@
-{
-  pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/4fe8d07066f6ea82cda2b0c9ae7aee59b2d241b3.tar.gz";
-    sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
-  }) {}
+{ lib
+, stdenv
+, fetchgit
+, macro11
 }:
-pkgs.stdenv.mkDerivation rec {
+
+stdenv.mkDerivation (finalAttrs: {
   pname = "macro11";
   version = "0.1.0";
 
-  src = pkgs.fetchgit {
+  src = fetchgit {
     url = "https://github.com/nigeleke/macro11";
     sha256 = "sha256-IcVUCBvTB/HtLdzd1wGhmUbvbvvQUf2Kpgcb1oQOkb4=";
   };
